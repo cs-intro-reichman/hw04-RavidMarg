17f9f2055cd57d323e0edfd3cde78497b243c90a
RavidMarg <ravid.margalit@post.runi.ac.il>
2024-01-13 15:02:59 +0200

    First commit

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,103 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int size = args.length;
+        int [] int_arr = new int [size];
+        for(int i=0; i<size; i++) {
+            int_arr[i] = Integer.parseInt(args[i]);
+        }
+        // testFindMissingInt(int_arr);
+        // testSecondMaxValue(int_arr);
+        // testContainsTheSameElements();
+        // testIsSorted(int_arr);
     }
-    
+
+    public static void testFindMissingInt (int [] test_array){
+        System.out.println(findMissingInt(test_array));
+    }
+
+    public static void testSecondMaxValue (int [] test_array){
+        System.out.println(secondMaxValue(test_array));
+    }
+
+    public static void testContainsTheSameElements (){
+        int [] r1 = {2, 1, 1, 2, 1};
+        int [] r2 = {2, 1};
+        int [] r3 = {5, 2, 1, -4, 3};
+        int [] r4 = {5, 1, -4, 3};
+        System.out.println(containsTheSameElements(r1, r2));
+        System.out.println(containsTheSameElements(r2, r1));
+        System.out.println(containsTheSameElements(r3, r4));
+        System.out.println(containsTheSameElements(r4, r3));
+    }
+
+    public static void testIsSorted (int [] test_array){
+        System.out.println(isSorted(test_array));
+    }
+
+    public static void sortArray (int [] array){
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (array[i] <= array[j])
+                {
+                    // swap array[i] and array[j]
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        sortArray(array);
+        int missing_int = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] + 1) != array[i + 1]){ // This function assumes that each number aapears once in the given array
+                missing_int = array[i] + 1;
+                break;
+            }
+        }
+        return missing_int;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        sortArray(array);
+        int array_second_last_location = array.length - 2;
+        return array[array_second_last_location];
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        int array1_length = array1.length;
+        int array2_length = array2.length;
+        int [] max_array = array1_length < array2_length ? array2 : array1;
+        int [] min_array = array1_length < array2_length ? array1 : array2;
+        int max_length = max_array.length;
+
+        for (int i = 0; i < max_array.length; i++) {
+            boolean found = false;
+            for (int j = 0; j < min_array.length; j++) {
+                if (max_array[i] == min_array[j]) {
+                    max_length--;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        return max_length == 0;
     }
 
+
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1])
+                return false;
+        }
+        return true;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,97 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        // System.out.println(capVowelsLowRest(args[0]));
+        // testAllIndexOf();
+    }
+
+    public static void testAllIndexOf(){
+        System.out.println(java.util.Arrays.toString(allIndexOf("Hello world",'l')));
+        System.out.println(java.util.Arrays.toString(allIndexOf("Hello worLd",'l')));
+        System.out.println(java.util.Arrays.toString(allIndexOf("Hello world",'o')));
+        System.out.println(java.util.Arrays.toString(allIndexOf("Hello world",' ')));
+        System.out.println(java.util.Arrays.toString(allIndexOf("Hello world",'d')));
+        System.out.println(java.util.Arrays.toString(allIndexOf("MMMM",'M')));
+    }
+
+    public static boolean isVowel(char test_char) {
+        return "aeiou".indexOf(test_char) != -1;
+    }
+
+    public static char toUpperCase(char char_to_upper ){
+        if (char_to_upper >= 97 && char_to_upper <= 122) {
+            char_to_upper = (char) (char_to_upper - 32);
+        }
+        return char_to_upper;
+    }
+
+    public static char toLowerCase(char char_to_lower ){
+        if (char_to_lower >= 65 && char_to_lower <= 90) {
+            char_to_lower = (char) (char_to_lower + 32);
+        }
+        return char_to_lower;
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String result = "";
+        for (int i = 0; i < string.length(); i++) {
+            char char_at_i = string.charAt(i);
+            if (isVowel(char_at_i)){
+                result = result + toUpperCase(char_at_i);
+            } else if ((char_at_i >= 67 && char_at_i <= 122) || (char_at_i >= 65 && char_at_i <= 90)) {
+                result = result + toLowerCase(char_at_i);
+            }
+            else {
+                result = result + char_at_i;
+            }
+        }
+        return result;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String result = "";
+        boolean is_first_word = true;
+        boolean last_char_is_space = false;
+        for (int i = 0; i < string.length(); i++) {
+            char char_at_i = string.charAt(i);
+            if (char_at_i == ' ') {
+                last_char_is_space = true;
+            } else if (is_first_word) {
+                result = result + toLowerCase(char_at_i);
+                is_first_word = false;
+                last_char_is_space = false;
+            } else if (last_char_is_space) {
+                result = result + toUpperCase(char_at_i);
+                last_char_is_space = false;
+            } else {
+                result = result + toLowerCase(char_at_i);
+            }
+        }
+        return result;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int [] possible_char_locations = new int [string.length()]; // max location == length of string (string contains single char multiple times)
+        int freq = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (chr == string.charAt(i)){
+                possible_char_locations[i] = i;
+                freq++;
+            }
+            else{
+                possible_char_locations[i] = -1;
+            }
+        }
+        int [] char_locations = new int [freq];
+        for (int i = 0; i < freq; i++) {
+            for (int j = 0; j < string.length(); j++) {
+                if ( possible_char_locations[j] != -1){
+                    char_locations[i] = possible_char_locations[j];
+                    possible_char_locations[j] = -1;
+                    break;
+                }
+            }
+        }
+        return char_locations;
     }
 }
